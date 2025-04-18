d7f1ad7e663a4ed1a0abfcfc98bb5d7f4a532940
Manikandan-5 <merndev05@gmail.com>
2025-04-18 16:54:24 +0530

    updatehome

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React from 'react';
+import backgroundVideo from '../assets/home.mp4'; // Ensure the correct path
 
 const Home = () => {
   return (
-    <div>
-      
-      
-    </div>
-  )
-}
+    <div className="home-container">
+      <video autoPlay muted loop className="background-video">
+        <source src={backgroundVideo} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
 
-export default Home
+      <div className="content">
+        <h1 className="animate-title font-futura font-semibold text-xl">Welcome To The InterSection</h1>
+        <h3 className="animate-line">Design Data Devics</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
